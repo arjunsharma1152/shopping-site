3b4bab002920474a418d21dd069c78a5c0685724
arjunsharma1152 <arjunsk923@gmail.com>
2022-09-05 23:30:54 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+
 import './App.css';
+
 import Homepage from './pages/homepage/homepage.component';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
-  );
+    <BrowserRouter>
+      <div>
+        <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route  path='/shop' component={ShopPage}/>
+       </Switch>
+       </div>
+    </BrowserRouter>
+  ); 
 }
 
 export default App;

841abd223b9a7d6b6071b655f59ad43c52152330
Arjun Sharma <arjunsk923@gmail.com>
2022-09-28 19:22:55 +0000

    Updated cart dropdown to display the items that are added in to the cart in the shop.

--- src/redux/cart/cart-action.js
+++ src/redux/cart/cart-action.js
@@ -1,5 +1,10 @@
 import CartActionTypes from "./cart-type";
 
 export const toggleHidden = () => ({
-    type: CartActionTypes.TOGGLE_HIDDEN
-})
+  type: CartActionTypes.TOGGLE_HIDDEN,
+});
+
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});

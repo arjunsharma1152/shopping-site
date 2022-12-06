1b32c877018f3a7ad8389eb2351044c7cdbace61
Arjun Sharma <arjunsk923@gmail.com>
2022-12-06 16:11:52 +0000

    Alan AI added in the app.

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import Directory from "../../components/directory/directory";
 import "./homepage.scss";
+import useAlan from "../../components/alanai/client-app";
 
-const Homepage = () => (
-  <div className="homepage">
-    <Directory />
-  </div>
-);
+const Homepage = () => {
+  const alanBtnContainer = useRef();
+
+  useAlan();
+  return (
+    <div className="homepage">
+      <Directory />
+      <div ref={alanBtnContainer} />
+    </div>
+  );
+};
 
 export default Homepage;
